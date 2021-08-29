bea41421967e4fd1d07206a4b5094d2340030670
Knexli <knexli6@gmail.com>
2021-08-29 19:26:44 +0200

    Add some very important changes

--- src/test/java/ExistingMapperReader.java
+++ src/test/java/ExistingMapperReader.java
@@ -1,17 +1,9 @@
 import org.apache.commons.lang3.StringUtils;
 
-import java.beans.IntrospectionException;
-import java.beans.Introspector;
-import java.beans.PropertyDescriptor;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.lang.reflect.ParameterizedType;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 
